6d4ce78187adef8d189689599a246bd7233edb45
Carlos Forero <ecarlosforero@gmail.com>
2010-11-04 17:50:56 -0430

    Added migrates for indexes of accounts, journals, journal_operations

---
+++ db/migrate/20101104220037_add_indexes_to_journals.rb
@@ -0,0 +1,15 @@
+class AddIndexesToJournals < ActiveRecord::Migration
+  def self.up
+    add_index :journals, :company_id
+    add_index :journals, :bill_id
+    add_index :journals, :period_id
+    add_index :journals, :journal_type_id
+  end
+
+  def self.down
+    remove_index :journals, :company_id
+    remove_index :journals, :bill_id
+    remove_index :journals, :period_id
+    remove_index :journals, :journal_type_id
+  end
+end
