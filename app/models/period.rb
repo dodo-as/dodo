385d82b6fa8fe9e4f2bbc0dfa15acc98318b6203
Zdena <zdena@satellite-pro.(none)>
2010-11-10 14:20:17 -0430

    improvement function in Period that return a periods range

--- app/models/period.rb
+++ app/models/period.rb
@@ -12,32 +12,35 @@ class Period < ActiveRecord::Base
   STATUSES = {0 => 'New', 1 => 'Open', 2 => 'Done', 3 => 'Closed'}
   STATUSE_NAMES = {'New' => 0, 'Open' => 1, 'Done' => 2, 'Closed' => 3}
 
-  # if (from_year, to_year) are nil, it will return all periods of the company
-  # if (to_year) are nil, it will return all periods until last period of the company
-  # if (from_year) are nil, it will return all periods since first period of the company
+  # options: from_year, from_nr, to_year, to_nr, company_id
+  # if (from_year, to_year) are nil, it will return all periods of the company from year 1900 to year 3999
+  # if (to_year) is nil, it will return all periods until last period of the company until year 3999
+  # if (from_year) is nil, it will return all periods since first period of the company since year 1900
+  # if (from_nr) is nil, it will return since nr 0
+  # if (to_nr) is nil, it will return until nr 9999
   # company is required
-  def self.get_range(from_year, from_nr, to_year, to_nr, company_id)
-    periods = Array.new
-    if from_year.blank? && to_year.blank?
-      periods.concat(where(['company_id = ?', company_id]))
-    else
-      unless from_year.blank? 
-        periods.concat(where(['company_id = ? and year = ? and nr >= ?', company_id, from_year, from_nr]))    
-      end
-      if !from_year.blank? && to_year.blank?
-        periods.concat(where(['company_id = ? and year > ? ', company_id, from_year]))
-      end
-      unless to_year.blank?
-        periods.concat(where(['company_id = ? and year = ? and nr <= ? ', company_id, to_year, to_nr]))
-      end
-      if from_year.blank? && !to_year.blank?
-        periods.concat(where(['company_id = ? and year < ? ', company_id, to_year]))
-      end
-      if !from_year.blank? && !to_year.blank?
-        periods.concat(where(['company_id = ? and year > ? and year < ? ', company_id, from_year, to_year])) if (to_year - from_year > 1)
-      end    
+  def self.get_range(company_id, options={})
+
+    raise "company is required to get_range" if company_id.blank?
+
+    options.delete_if { |key, value| value.blank? } 
+    options[:from_year] ||= 1900    
+    options[:from_nr] ||= 0
+    options[:to_year] ||= 3999
+    options[:to_nr] ||= 9999
+    
+    periods = []
+
+    if options[:from_year] == options[:to_year]
+      periods.concat(where(['company_id = ? and year = ? and nr >= ? and nr <= ?', company_id, options[:from_year], options[:from_nr], options[:to_nr]]))    
+    else  
+      periods.concat(where(['company_id = ? and year = ? and nr >= ?', company_id, options[:from_year], options[:from_nr]]))    
+      periods.concat(where(['company_id = ? and year = ? and nr <= ? ', company_id, options[:to_year], options[:to_nr]]))    
+      periods.concat(where(['company_id = ? and year > ? and year < ? ', company_id, options[:from_year], options[:to_year]])) if (options[:to_year] - options[:from_year] > 1)
     end
+
     return periods
+
   end
 
   def status_name
