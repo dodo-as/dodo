fd148c1b7709f16ab380d6190901a4471c8f807a
Paal de Vibe <paal@notadomainafaik.com>
2010-12-09 16:20:42 +0100

    fix too long index name

--- db/migrate/20100831220327_create_weekly_sale_shift_liquids.rb
+++ db/migrate/20100831220327_create_weekly_sale_shift_liquids.rb
@@ -6,7 +6,7 @@ class CreateWeeklySaleShiftLiquids < ActiveRecord::Migration
       t.float :amount, :null=>false, :default=>0.0     
       t.timestamps
     end
-    add_index :weekly_sale_shift_liquids, [:weekly_sale_shift_id, :weekly_sale_setup_liquid_id], :unique => true
+    add_index :weekly_sale_shift_liquids, [:weekly_sale_shift_id, :weekly_sale_setup_liquid_id], :name => "index_weekly_sale_shift_liquids_shift_setup_liquid", :unique => true
 
     execute "ALTER TABLE weekly_sale_shift_liquids ADD CONSTRAINT fk_weekly_sale_shift_liquids_weekly_sale_shift FOREIGN KEY (weekly_sale_shift_id)  REFERENCES weekly_sale_shifts(id) ON DELETE CASCADE"
     execute "ALTER TABLE weekly_sale_shift_liquids ADD CONSTRAINT fk_weekly_sale_shift_liquids_weekly_sale_setup_liquid FOREIGN KEY (weekly_sale_setup_liquid_id)  REFERENCES weekly_sale_setup_liquids(id) "
