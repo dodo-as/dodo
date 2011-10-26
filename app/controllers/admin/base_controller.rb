e363faa5c80c0539ecbb650f5e255312c25d82c3
Nikola Trandafilovic <noostranik@lodo-dev.freecode.no>
2011-10-26 19:27:47 +0200

    Added filter_resource_acces for admin backend (work in progress)

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,16 +1,30 @@
 # Checks if user has permission to access administration
 class Admin::BaseController < ApplicationController
   skip_before_filter :authenticate_user!, :init_auth, :company_required
-  before_filter :authenticate_admin!, :init_admin_auth
+  before_filter   :init_admin_auth
 
+    filter_resource_access
 
   # shouldn't be neccessary as we only have one admin level
   # filter_access_to :all
+    puts "BBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCccccccccccc"
   
   protected
 
   def init_admin_auth
-    Authorization.current_user = current_admin
+    puts "BBBBBBBBBBBBBBBBBBBB", current_admin, current_user
+    if current_admin
+        Authorization.current_user = current_admin
+    elsif current_user
+        if current_user.is_user_admin
+            Authorization.current_user = current_user
+        end
+    end
+        
   end
+  #~ 
+  #~ def
+    #~ if current_user
+  #~ end
 
 end
