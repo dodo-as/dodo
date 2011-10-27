d7266f50e1cef7bacd1c20887a1af65fe673fabb
Nikola Trandafilovic <noostranik@lodo-dev.freecode.no>
2011-10-27 10:40:45 +0200

    User administration role code is comented out, and issue us put on hold (refs #3940)

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,29 +1,28 @@
 # Checks if user has permission to access administration
 class Admin::BaseController < ApplicationController
   skip_before_filter :authenticate_user!, :init_auth, :company_required
-  before_filter   :init_admin_auth
+  before_filter  :authenticate_admin!, :init_admin_auth
 
     filter_resource_access
 
   # shouldn't be neccessary as we only have one admin level
   # filter_access_to :all
-    puts "BBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCccccccccccc"
   
   protected
 
   def init_admin_auth
-    puts "BBBBBBBBBBBBBBBBBBBB", current_admin, current_user
-    if current_admin
-        Authorization.current_user = current_admin
-    elsif current_user
-        if current_user.is_user_admin
-            Authorization.current_user = current_user
-        end
-    end
-        
+    Authorization.current_user = current_admin
   end
   #~ 
   #~ def
+    #~ puts "BBBBBBBBBBBBBBBBBBBB", current_admin, current_user
+    #~ if current_admin
+    #~ elsif current_user
+        #~ if current_user.is_user_admin
+            #~ Authorization.current_user = current_user
+        #~ end
+    #~ end
+        #~ 
     #~ if current_user
   #~ end
 
